a7f048f4a2b3667798584763a66aa44964523085
Székely Márton <53979153+PiciAkk@users.noreply.github.com>
2021-11-20 14:19:23 +0100

    Create pystatistics.py

---
+++ pystatistics.py
@@ -0,0 +1,46 @@
+from statistics import median
+
+class stats:
+    def __init__(self, lst):
+        self.lst = lst
+        self.sortedList = sorted(lst)
+    def average(self):
+        return (sum(self.lst) / len(self.lst))
+    def median(self):
+        return median(self.lst)
+    def firstQuartile(self):
+        upToMedian = []
+        for element in self.sortedList:
+            if element < self.median():
+                upToMedian.append(element)
+        return median(upToMedian)
+    def thirdQuartile(self):
+        fromMedian = []
+        for element in self.sortedList:
+            if element > self.median():
+                fromMedian.append(element)
+        return median(fromMedian)
+    def mode(self):
+        modes = []
+        occurrences = {}
+        maximumOccurrence = 0
+        for element in self.lst:
+            if element not in occurrences:
+                occurrences.update({element: 1})
+            else:
+                occurrences[element] += 1
+        for number, occurrence in occurrences.items():
+            if occurrence > maximumOccurrence:
+                maximumOccurrence = occurrence
+        for number, occurrence in occurrences.items():
+            if occurrence == maximumOccurrence:
+                modes.append(number)
+        if len(modes) == 1:
+            return modes[0]
+        else:
+            return modes
+    def max(self):
+        return self.sortedList[len(self.sortedList)-1]
+        # or we can just do self.sortedList[-1]
+    def min(self):
+        return self.sortedList[0]
